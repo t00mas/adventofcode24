56d12bb88ea12002b026223758af0af5e0c64a5d
Tomas Pica <tomas.picadelcanto@grafana.com>
2024-12-02 15:49:05 +0100

    template

---
+++ day-template/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+)
+
+//go:embed input.txt
+var input_txt []byte
+
+func main() {
+	//input := string(input_txt)
+	//lines := strings.Split(input, "\n")
+
+	fmt.Println("Part 1: ")
+
+	fmt.Println("Part 2: ")
+}
